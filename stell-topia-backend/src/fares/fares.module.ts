e00273fe55df0c56fb446546bae23d63e77c6b3d
Qoder-Undefined <devdija3@gmail.com>
2026-08-09 08:04:04 +0100

    fares module

---
+++ stell-topia-backend/src/fares/fares.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FaresService } from './fares.service';
+import { FaresController } from './fares.controller';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [FaresController],
+  providers: [FaresService],
+  exports: [FaresService],
+})
+export class FaresModule {}
