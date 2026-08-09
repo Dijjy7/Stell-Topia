7bc1db118e9f5d78541a11aa0493892b407c5cc7
Qoder-Undefined <devdija3@gmail.com>
2026-08-09 08:05:49 +0100

    update booking controller

--- stell-topia-backend/src/booking/booking.module.ts
+++ stell-topia-backend/src/booking/booking.module.ts
@@ -3,9 +3,10 @@ import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { FaresModule } from '../fares/fares.module';
 import { StellarModule } from '../stellar/stellar.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [FaresModule, StellarModule],
+  imports: [FaresModule, StellarModule, AuthModule],
   controllers: [BookingController],
   providers: [BookingService],
   exports: [BookingService],
