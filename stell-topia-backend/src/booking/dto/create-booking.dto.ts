16f95358439fe5b3670b0f0be17a302258a5f42a
Qoder-Undefined <devdija3@gmail.com>
2026-08-09 08:00:14 +0100

    slo

---
+++ stell-topia-backend/src/booking/dto/create-booking.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsInt, IsNotEmpty } from 'class-validator';
+
+export class CreateBookingDto {
+  @IsString()
+  @IsNotEmpty()
+  flightId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  from: string;
+
+  @IsString()
+  @IsNotEmpty()
+  to: string;
+
+  @IsString()
+  @IsNotEmpty()
+  departure_date: string;
+
+  @IsInt()
+  @Min(1)
+  passengers: number;
+}
