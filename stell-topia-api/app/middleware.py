29ee21baf4e39f6fef2b1c1754d1fd079171b20b
Qoder-Undefined <devdija3@gmail.com>
2026-08-11 10:30:31 +0100

    updated the middleware file

--- stell-topia-api/app/middleware.py
+++ stell-topia-api/app/middleware.py
@@ -1,7 +1,7 @@
 import time
 import uuid
 from collections.abc import Callable
-from typing import Any
+from typing import Any, cast
 
 from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -12,7 +12,7 @@ class RequestIdMiddleware(BaseHTTPMiddleware):
         request_id = request.headers.get("x-request-id", str(uuid.uuid4()))
         request.state.request_id = request_id
         start = time.perf_counter()
-        response = await call_next(request)
+        response = cast(Response, await call_next(request))
         duration_ms = int((time.perf_counter() - start) * 1000)
         response.headers["x-request-id"] = request_id
         response.headers["x-process-time-ms"] = str(duration_ms)
