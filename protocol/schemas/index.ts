0a9e1172a74a5fa398f9b77ca24f6c16b41e2939
Qoder-Undefined <devdija3@gmail.com>
2026-08-12 09:23:55 +0100

    updated index

--- protocol/schemas/index.ts
+++ protocol/schemas/index.ts
@@ -7,6 +7,9 @@ export type { Hash32 } from "./hash";
 export { stroopAmountSchema } from "./amount";
 export type { StroopAmount } from "./amount";
 
+
+
+
 export {
   EnvelopeSchema,
   type Envelope,
